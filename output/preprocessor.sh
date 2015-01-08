fc240620344f69b9ce439ab39e3376cf8a9b6e5c
Lighton Phiri <lighton.phiri@gmail.com>
2015-01-08 15:05:36 +0200

    Added new sample plots
    Added a flipped barplot example
    Added a boxplot example

--- output/preprocessor.sh
+++ output/preprocessor.sh
@@ -2,19 +2,26 @@
 # 
 # Clean up automatically create files and rename file using conventional naming scheme
 
+latex --shell-escape r-plot-samples.tex
+
 filename=r-plot-samples-figure;
 counter=0;
 for file in `cat r-plot-samples.tex | grep input | sed -e 's/\\\input{\(.*\)}/\1/' | tr -d ' '`; do 
+	rm $filename$counter".dvi";
+	rm $filename$counter".ps";
+	rm $filename$counter".md5";
+	rm $filename$counter".dpth";
+	rm $filename$counter".log";
 	echo $file;
 	echo $file | awk -F.tex '{print $1".eps"}';
 	echo $filename$counter;
 	epsfile=`echo $file | awk -F.tex '{print $1".eps"}'`;
 	echo $epsfile;
 	mv $filename$counter".eps" $epsfile;
-	#rm $filename$counter".dvi";
-        #rm $filename$counter".ps";
-	#rm $filename$counter".md5";
-	#rm $filename$counter".dpth";
-	#rm $filename$counter".log";
+	rm $filename$counter".dvi";
+        rm $filename$counter".ps";
+	rm $filename$counter".md5";
+	rm $filename$counter".dpth";
+	rm $filename$counter".log";
 	counter=$[$counter + 1]; 
 done
